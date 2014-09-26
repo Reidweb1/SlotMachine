35245c4d33ca304431d821cf18aaa6986af9afb4
Reid Weber <reidweber@Reids-MacBook-Air.local>
2014-09-26 11:55:14 -0700

    MasterCopy

---
+++ Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Reid Weber on 9/26/14.
+//  Copyright (c) 2014 com.reidweber. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+	var value = 0
+	var image: UIImage = UIImage(named: "Ace")
+	var isRed: Bool = true
+}
